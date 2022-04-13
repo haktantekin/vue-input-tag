48a79014c8778a0ea288dfd656e3c5e07dc8b7d2
haktan-tekin <webhaktan@gmail.com>
2022-04-13 13:15:29 +0300

    repeat data control and show error message

--- src/App.vue
+++ src/App.vue
@@ -5,19 +5,40 @@
       <span class="close">x</span>
     </span>
     <input type="text" @keydown.enter="addTag">
+    <div class="error" v-if="error">Bu etiket eklenmiş!</div>
   </div>
 </template>
 <script>
 export default{
   data(){
     return{
-      tags:["deneme","test"]
+      tags:["deneme","test"],
+      error: false
     }
   },
   methods:{
     addTag(event){
-      this.tags.push(event.target.value);
-      event.target.value='';
+      let text = event.target.value;
+      let matchTag = false
+      if(text.length > 0){
+
+        this.tags.forEach(tag => {
+          if(tag.toLowerCase() === text.toLowerCase()){
+            matchTag = true
+          }
+        })
+
+        if(!matchTag){
+          this.tags.push(text);
+          text = '';
+        }
+        else{
+          this.error = true;
+          setTimeout(()=>{
+            this.error = false;
+          },2000)
+        }
+      }
     }
   }
 }
@@ -43,6 +64,11 @@ export default{
     outline: nnone;
     width: 100px;
     height:30px;
+  }
+  .error{
+    font-size:12px;
+    color:red;
+    margin-top:15px;
   }
   
   .tag .close{
